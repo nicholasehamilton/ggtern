a9494b71520d65b0d2217f31893d66ffd6dbd53c
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-05 13:27:39 +1100

    Renamed plot-build to ggtern-build

--- R/ggtern-build.R
+++ R/ggtern-build.R
@@ -9,17 +9,18 @@ ggtern_build <- function(plot){
     tryCatch({
       PADDING <- max(plot$theme$ternary.options$padding, 0)
       xlim <- c(0 - PADDING, 1 + PADDING)
-      ylim <- c(as.numeric(transformTernToCart(-PADDING,0,   PADDING,scale=F)[2]),
-                as.numeric(transformTernToCart( 1+PADDING,0,-PADDING,scale=F)[2]))
+      ylim <- c(as.numeric(transform_tern_to_cart(-PADDING,0,   PADDING,scale=F)[2]),
+                as.numeric(transform_tern_to_cart( 1+PADDING,0,-PADDING,scale=F)[2]))
     },error=function(e){
       #NOTHING
     })
     
     ##Add the ternary fixed coordinates
+    ##One doesn't exist. So we create.
     if(!inherits(plot$coordinates,"ternary")){plot <- plot + coord_tern(xlim=xlim,ylim=ylim)}
     
     ##Update the coordinates limits.
-    for(X in c("T","L","R")){ plot$coordinates$limits[[X]] <- plot$scales$get_scales(X)$limits}
+    for(X in c("T","L","R")){plot$coordinates$limits[[X]] <- plot$scales$get_scales(X)$limits}
     
     #THE TERNARY COMPONENTS 
     tern.layer <- list(  ternbackground(plot)
