3a32148ca1de37118da2dc9e100000ced6bbf452
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-11 20:30:02 +1100

    Fixed .onLoad

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,20 +1,4 @@
 .onLoad <- function(libname, pkgname){
-  
-  # do whatever needs to be done when the package is loaded
-  # some people use it to bombard users with 
-  # messages using 
-  packageStartupMessage("")
-  packageStartupMessage("+--------------------------------------------------------+")
-  packageStartupMessage("|             Ternary Plots in R using ggplot2           |")
-  packageStartupMessage("+--------------------------------------------------------+")
-  packageStartupMessage("| Features Include:                                      |")
-  packageStartupMessage("|  + Extension to ggplot2 for ternary diagrams           |")
-  packageStartupMessage("|                                                        |")
-  packageStartupMessage("+--------------------------------------------------------+")
-  
-  #Update ggplot2 Methods.
   suppressWarnings(.push_tweaks())
-  
-  #Global Setting.
   options("tern.discard.external"=TRUE)
 }
