0b86cd00965df31b1688ae142f749f716b135019
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-13 09:34:11 +1100

    1.0.0.4 Final

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @name Feldspar
+#' @title Elkin and Groves Feldspar Data
+#' @description This dataset is some data on Feldspar and its phases, as a function of temperature and pressure.
+#' @docType data
+#' @usage data(Feldspar)
+#' @format 1 row per Feldspar composition
+#' @source Elkin and Groves, 1990
+#' @author Nick Hamilton
+#' @export
+NULL
