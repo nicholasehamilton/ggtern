75b40acedfaba4b8926e6dd3f35f95ee1ff6cfc7
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-14 12:29:03 +1100

    removed .ggplot_build and .all_aesthetics assignInNamespace

--- R/modified-original.R
+++ R/modified-original.R
@@ -1,19 +1,22 @@
 .push_tweaks <- function(){
-  #MAIN BUILD FUNCTION
-  assignInNamespace("ggplot_build",ggplot_build,"ggplot2")
-  
   #THEME ELEMENTS
   assignInNamespace(".element_tree",.element_tree,"ggplot2")
   assignInNamespace("theme_get",.theme_get,"ggplot2")
   assignInNamespace("theme_set",.theme_set,"ggplot2")
-  assignInNamespace(".all_aesthetics",.all_aesthetics,"ggplot2")
+  
+  assignInNamespace("StatSmooth",.StatSmooth,"ggplot2")
   
   #GEOMETRIES ETC.
-  assignInNamespace("StatSmooth",.StatSmooth,"ggplot2")
   assignInNamespace("GeomSegment",.GeomSegment,"ggplot2")
   assignInNamespace("StatDensity2d",ggint$StatDensity2d,"ggplot2")
 
   assignInNamespace("panel_scales",.panel_scales,"ggplot2")
   assignInNamespace("train_ranges",ggint$train_ranges,"ggplot2")
 }
+
+
+
+#MAIN BUILD FUNCTION
+#assignInNamespace("ggplot_build",ggplot_build,"ggplot2")
+#assignInNamespace(".all_aesthetics",.all_aesthetics,"ggplot2")
 
