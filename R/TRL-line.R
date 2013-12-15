622817e435e509eeb4f2993a6254b5aa353831b9
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-16 10:44:14 +1100

    Tline Lline and Rline convenience functions

---
+++ R/TRL-line.R
@@ -0,0 +1,8 @@
+#' @rdname statTLRline
+Tline <- function(Tintercept=NULL,...){geom_Tline(Tintercept=Tintercept,...)}
+
+#' @rdname statTLRline
+Lline <- function(Lintercept=NULL,...){geom_Lline(Lintercept=Lintercept,...)}
+
+#' @rdname statTLRline
+Rline <- function(Rintercept=NULL,...){geom_Rline(Rintercept=Rintercept,...)}
--- R/geom-Tline.R
+++ R/geom-Tline.R
@@ -13,11 +13,20 @@
 #' @param Tintercept for geom_Tline and stat_Tline, the constant value of the THS apex constituent.
 #' @param Lintercept for geom_Lline and stat_Lline, the constant value of the LHS apex constituent.
 #' @param Rintercept for geom_Rline and stat_Rline, the constant value of the RHS apex constituent.
+#' @section Tline, Lline and Rline:
+#' Convenience functions, \code{Tline(...), Lline(...)} and \code{Rline(...)} have been created so that constant lines can be quickly created:
 #' @examples
+#' #Base usage
 #' ggtern()  + 
 #'  geom_Lline(Lintercept=0.5,color="red") + 
 #'  geom_Tline(Tintercept=0.5,color="green") + 
 #'  geom_Rline(Rintercept=0.5,color="blue")
+#'  
+#' #Convenience Functions
+#' ggtern() + 
+#'  Tline(0.5) + 
+#'  Lline(0.5) + 
+#'  Rline(0.5)
 #' @export
 NULL
 
