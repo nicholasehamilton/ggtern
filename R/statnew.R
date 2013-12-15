c0446988bc2636f3d1c91963b8d0a0b18b9ed17c
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-15 13:39:05 +1100

    Creation of Statnew Proto

---
+++ R/statnew.R
@@ -0,0 +1,60 @@
+Statnew <- proto(ggint$TopLevel, expr={
+  objname <- "" 
+  desc <- ""
+  
+  # Should the values produced by the statistic also be transformed
+  # in the second pass when recently added statistics are trained to 
+  # the scales
+  retransform <- TRUE
+  
+  default_geom <- function(.) Geom
+  default_aes <- function(.) aes()
+  default_pos <- function(.) .$default_geom()$default_pos()
+  required_aes <- c()
+  
+  aesthetics <- list()
+  calculate <- function(., data, scales, ...) {
+    writeLines("Original Calculate")
+  }
+  
+  calculate_groups <- function(., data, scales, ...) {
+    if (empty(data)) return(data.frame())
+    
+    force(data)
+    force(scales)
+    
+    groups <- split(data, data$group)
+    stats <- lapply(groups, function(group) 
+      .$calculate(data = group, scales = scales, ...))
+    
+    stats <- mapply(function(new, old) {
+      if (empty(new)) return(data.frame())
+      unique <- uniquecols(old)
+      missing <- !(names(unique) %in% names(new))
+      cbind(
+        new, 
+        unique[rep(1, nrow(new)), missing,drop=FALSE]
+      )
+    }, stats, groups, SIMPLIFY=FALSE)
+    
+    do.call(rbind.fill, stats)
+  }
+  
+  
+  pprint <- function(., newline=TRUE) {
+    cat("stat_", .$objname ,": ", sep="") # , clist(.$parameters())
+    if (newline) cat("\n")
+  }
+  
+  parameters <- function(.) {
+    params <- formals(get("calculate", .))
+    params[setdiff(names(params), c(".","data","scales"))]
+  }
+  
+  class <- function(.) "stat"
+  
+  new <- function(., mapping=aes(), data=NULL, geom=NULL, position=NULL, ...){
+    do.call("layer", list(mapping=mapping, data=data, geom=geom, stat=., position=position, ...))
+  }
+  
+})
