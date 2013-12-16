6d3fb1228e62830d806f63b81be81e4d9f731f88
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-16 17:00:03 +1100

    .element_tree -> ggint$.element_tree

--- R/theme.R
+++ R/theme.R
@@ -76,59 +76,59 @@
 NULL
 
 #SEARCH FOR THE ORIGINAL FUNCTIONS
-.element_tree <- find_global(".element_tree")
+ggint$.element_tree <- find_global(".element_tree")
 .el_def       <- find_global("el_def")
 
 #TERNARY OPTIONS.
-.element_tree$ternary.options = .el_def("element_ternary","") #
+ggint$.element_tree$ternary.options = .el_def("element_ternary","element_ternary") #
 
 ##TERNARY PANEL
-.element_tree$panel.background.tern <- .el_def("element_rect", "rect")
+ggint$.element_tree$panel.background.tern <- .el_def("element_rect", "rect")
 
 ##AXIS ARROWS
-.element_tree$axis.tern         = .el_def("element_line", "line") #
-.element_tree$axis.tern.arrow   = .el_def("element_line", "axis.tern") #
-.element_tree$axis.tern.arrow.T = .el_def("element_line", "axis.tern.arrow") #
-.element_tree$axis.tern.arrow.L = .el_def("element_line", "axis.tern.arrow") #
-.element_tree$axis.tern.arrow.R = .el_def("element_line", "axis.tern.arrow") #
-.element_tree$axis.tern.line    = .el_def("element_line", "axis.tern") #
-.element_tree$axis.tern.line.T  = .el_def("element_line", "axis.tern.line") #
-.element_tree$axis.tern.line.L  = .el_def("element_line", "axis.tern.line") #
-.element_tree$axis.tern.line.R  = .el_def("element_line", "axis.tern.line") #
-.element_tree$axis.tern.text    = .el_def("element_text", "text") #
-.element_tree$axis.tern.text.T  = .el_def("element_text", "axis.tern.text") #
-.element_tree$axis.tern.text.L  = .el_def("element_text", "axis.tern.text") #
-.element_tree$axis.tern.text.R  = .el_def("element_text", "axis.tern.text") #
-.element_tree$axis.tern.arrow.text     = .el_def("element_text", "axis.tern.text") #
-.element_tree$axis.tern.arrow.text.T   = .el_def("element_text", "axis.tern.arrow.text") #
-.element_tree$axis.tern.arrow.text.L   = .el_def("element_text", "axis.tern.arrow.text") #
-.element_tree$axis.tern.arrow.text.R   = .el_def("element_text", "axis.tern.arrow.text") #
-.element_tree$axis.tern.title          = .el_def("element_text", "axis.tern.text")#
-.element_tree$axis.tern.title.T        = .el_def("element_text", "axis.tern.title")#
-.element_tree$axis.tern.title.L        = .el_def("element_text", "axis.tern.title")#
-.element_tree$axis.tern.title.R        = .el_def("element_text", "axis.tern.title")#
+ggint$.element_tree$axis.tern         = .el_def("element_line", "line") #
+ggint$.element_tree$axis.tern.arrow   = .el_def("element_line", "axis.tern") #
+ggint$.element_tree$axis.tern.arrow.T = .el_def("element_line", "axis.tern.arrow") #
+ggint$.element_tree$axis.tern.arrow.L = .el_def("element_line", "axis.tern.arrow") #
+ggint$.element_tree$axis.tern.arrow.R = .el_def("element_line", "axis.tern.arrow") #
+ggint$.element_tree$axis.tern.line    = .el_def("element_line", "axis.tern") #
+ggint$.element_tree$axis.tern.line.T  = .el_def("element_line", "axis.tern.line") #
+ggint$.element_tree$axis.tern.line.L  = .el_def("element_line", "axis.tern.line") #
+ggint$.element_tree$axis.tern.line.R  = .el_def("element_line", "axis.tern.line") #
+ggint$.element_tree$axis.tern.text    = .el_def("element_text", "text") #
+ggint$.element_tree$axis.tern.text.T  = .el_def("element_text", "axis.tern.text") #
+ggint$.element_tree$axis.tern.text.L  = .el_def("element_text", "axis.tern.text") #
+ggint$.element_tree$axis.tern.text.R  = .el_def("element_text", "axis.tern.text") #
+ggint$.element_tree$axis.tern.arrow.text     = .el_def("element_text", "axis.tern.text") #
+ggint$.element_tree$axis.tern.arrow.text.T   = .el_def("element_text", "axis.tern.arrow.text") #
+ggint$.element_tree$axis.tern.arrow.text.L   = .el_def("element_text", "axis.tern.arrow.text") #
+ggint$.element_tree$axis.tern.arrow.text.R   = .el_def("element_text", "axis.tern.arrow.text") #
+ggint$.element_tree$axis.tern.title          = .el_def("element_text", "axis.tern.text")#
+ggint$.element_tree$axis.tern.title.T        = .el_def("element_text", "axis.tern.title")#
+ggint$.element_tree$axis.tern.title.L        = .el_def("element_text", "axis.tern.title")#
+ggint$.element_tree$axis.tern.title.R        = .el_def("element_text", "axis.tern.title")#
 
-.element_tree$axis.tern.ticks          = .el_def("element_line", "axis.tern")#
-.element_tree$axis.tern.ticks.major    = .el_def("element_line", "axis.tern.ticks")#
-.element_tree$axis.tern.ticks.major.T  = .el_def("element_line", "axis.tern.ticks.major")#
-.element_tree$axis.tern.ticks.major.L  = .el_def("element_line", "axis.tern.ticks.major")#
-.element_tree$axis.tern.ticks.major.R  = .el_def("element_line", "axis.tern.ticks.major")#
+ggint$.element_tree$axis.tern.ticks          = .el_def("element_line", "axis.tern")#
+ggint$.element_tree$axis.tern.ticks.major    = .el_def("element_line", "axis.tern.ticks")#
+ggint$.element_tree$axis.tern.ticks.major.T  = .el_def("element_line", "axis.tern.ticks.major")#
+ggint$.element_tree$axis.tern.ticks.major.L  = .el_def("element_line", "axis.tern.ticks.major")#
+ggint$.element_tree$axis.tern.ticks.major.R  = .el_def("element_line", "axis.tern.ticks.major")#
 
-.element_tree$axis.tern.ticks.minor    = .el_def("element_line", "axis.tern.ticks")#
-.element_tree$axis.tern.ticks.minor.T  = .el_def("element_line", "axis.tern.ticks.minor")#
-.element_tree$axis.tern.ticks.minor.L  = .el_def("element_line", "axis.tern.ticks.minor")#
-.element_tree$axis.tern.ticks.minor.R  = .el_def("element_line", "axis.tern.ticks.minor")#
+ggint$.element_tree$axis.tern.ticks.minor    = .el_def("element_line", "axis.tern.ticks")#
+ggint$.element_tree$axis.tern.ticks.minor.T  = .el_def("element_line", "axis.tern.ticks.minor")#
+ggint$.element_tree$axis.tern.ticks.minor.L  = .el_def("element_line", "axis.tern.ticks.minor")#
+ggint$.element_tree$axis.tern.ticks.minor.R  = .el_def("element_line", "axis.tern.ticks.minor")#
 
-.element_tree$panel.grid.tern          = .el_def("element_line", "axis.tern") #
-.element_tree$panel.grid.tern.major    = .el_def("element_line", "panel.grid.tern") #
-.element_tree$panel.grid.tern.major.T  = .el_def("element_line", "panel.grid.tern.major") #
-.element_tree$panel.grid.tern.major.L  = .el_def("element_line", "panel.grid.tern.major") #
-.element_tree$panel.grid.tern.major.R  = .el_def("element_line", "panel.grid.tern.major") #
+ggint$.element_tree$panel.grid.tern          = .el_def("element_line", "axis.tern") #
+ggint$.element_tree$panel.grid.tern.major    = .el_def("element_line", "panel.grid.tern") #
+ggint$.element_tree$panel.grid.tern.major.T  = .el_def("element_line", "panel.grid.tern.major") #
+ggint$.element_tree$panel.grid.tern.major.L  = .el_def("element_line", "panel.grid.tern.major") #
+ggint$.element_tree$panel.grid.tern.major.R  = .el_def("element_line", "panel.grid.tern.major") #
 
-.element_tree$panel.grid.tern.minor    = .el_def("element_line", "panel.grid.tern") #
-.element_tree$panel.grid.tern.minor.T  = .el_def("element_line", "panel.grid.tern.minor") #
-.element_tree$panel.grid.tern.minor.L  = .el_def("element_line", "panel.grid.tern.minor") #
-.element_tree$panel.grid.tern.minor.R  = .el_def("element_line", "panel.grid.tern.minor") #
+ggint$.element_tree$panel.grid.tern.minor    = .el_def("element_line", "panel.grid.tern") #
+ggint$.element_tree$panel.grid.tern.minor.T  = .el_def("element_line", "panel.grid.tern.minor") #
+ggint$.element_tree$panel.grid.tern.minor.L  = .el_def("element_line", "panel.grid.tern.minor") #
+ggint$.element_tree$panel.grid.tern.minor.R  = .el_def("element_line", "panel.grid.tern.minor") #
 
 
 #internal kills the cartesian elements.
