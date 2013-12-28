ec4e25fa486076452e8e505a39d32a17dce374ae
nicholasehamilton <nicholasehamilton@gmail.com>
2013-12-28 12:10:00 +1100

    tern_stop() function created.

---
+++ R/tern-stop.R
@@ -0,0 +1,12 @@
+
+#' Stop Procedure
+#' 
+#' Internal Function, checks if the most recent coordinate system is ternary, and, if not, 
+#' stops the current procedure, with a common message format
+#' @param src character name of current procedure 
+tern_stop <- function(src="target"){
+  lc <- get_last_coord()
+  if(!inherits(lc,"ternary")){
+    stop(paste(src,"only relevant for ternary coordinates."),call.=FALSE)
+  }
+}
