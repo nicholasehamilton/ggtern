369fad480f1efdd02f8fc5383fb5cc87f0d2136b
nicholasehamilton <nicholasehamilton@gmail.com>
2014-01-22 14:37:27 +1100

    Documentation

--- R/geom-density2d.R
+++ R/geom-density2d.R
@@ -3,15 +3,11 @@
 #' Perform a 2D kernel density estimatation using kde2d and display the
 #' results with contours.
 #' 
-#' This can be useful for dealing with overplotting. Additional weight aesthetic permits better weighting if desired
+#' This can be useful for dealing with overplotting. Additional weight aesthetic (see aesthetic section below) permits better weighting if desired
 #'
 #' @aliases Density2dTern
 #' @section Aesthetics: 
-#' \Sexpr[results=rd,stage=build]{ggplot2:::rd_aesthetics("geom", "density2d")}
-#' Additional optional aethetic in the patched version (if not provided, existing behaviour applies)
-#' \itemize{
-#'  \item \code{weight}
-#' }
+#' \Sexpr[results=rd,stage=build]{ggtern:::rd_aesthetics("geom", "density2dtern")}
 #' @inheritParams ggplot2::geom_point
 #' @inheritParams ggplot2::geom_path
 #' @aliases GeomDensity2dtern
@@ -25,5 +21,5 @@ geom_density2d <- function (mapping = NULL, data = NULL, stat = "density2dtern",
 GeomDensity2dtern <- proto(ggint$GeomPath,{
   objname <- "density2dtern"
   default_stat <- function(.) StatDensity2dNew
-  default_aes <- function(.) aes(colour="#3366FF", size = 0.5, linetype = 1, alpha = NA)
+  default_aes <- function(.) aes(colour="#3366FF", size = 0.5, linetype = 1, alpha = NA,weight=NULL)
 })
