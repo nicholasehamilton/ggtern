c4e3cfd58e6f48e36e8b1aba74e4b217e537ebae
Nicholas Hamilton <nicholasehamilton@gmail.com>
2015-08-02 14:01:43 +1000

    Called Draw method from polygon_tern from geom_confidence_tern and geom_density_tern

--- R/geom-confidence.R
+++ R/geom-confidence.R
@@ -18,59 +18,29 @@ geom_confidence <- function (mapping = NULL, data = NULL,breaks=c(0.50,0.90,0.95
 }
 
 GeomConfidence <- proto(Geom, {
-  objname <- "confidence"
-  draw <- function(.,data,scales,coordinates,arrow = NULL,lineend = "butt",na.rm = FALSE,...){    
+  objname      <- "confidence"
+  required_aes <- c("x", "y")
+  default_stat <- function(.) StatConfidence
+  default_aes  <- function(.) aes(colour="#3366FF", size=0.5,linetype=1,alpha = NA,fill=NA)
+  guide_geom   <- function(.) "path"
+  draw         <- function(.,
+                   data,
+                   scales,
+                   coordinates,
+                   arrow     = NULL,
+                   lineend   = "butt",
+                   linejoin  = "round",
+                   linemitre = 1,
+                   na.rm     = FALSE,...){    
     ##DO THE VARIABLE AESTHETIC CHECK x and y for cartesian, and x,y,z for ternary...
     required_aes <- sort(unique(c(.$required_aes,coordinates$required_aes)))
     check_required_aesthetics(required_aes, names(data),"geom_confidence")
-    ##REMOVE MISSING DATA.
-    data <- remove_missing(data, na.rm = na.rm,c(required_aes),name = "geom_confidence")
-    if(empty(data)) return(.zeroGrob)
     
-    #Check
-    if(!inherits(coordinates,"ternary")){
-      warning("Coordinates Must be Ternary, returning zerogrob")
-      return(.zeroGrob)
-    }
-    
-    #Which to Ply On 
-    plyon = c("PANEL","group")
-    
-    ##Build the List of Grobs in Sequence
-    contours = dlply(data,plyon,function(df.orig){
-      
-      #Kill the Colours
-      df  <- suppressColours(df.orig,coordinates,remove=FALSE)
-      
-      #Default Grobs
-      polygrob = pathgrob = .zeroGrob
-      
-      if(nrow(df) > 0){
-        #Polygons, with no border (color)
-        df.poly <- subset(df,!(fill == 'transparent') | fill == NA)
-        if(nrow(df.poly) > 0){
-          df.poly$colour <- NA
-          polygrob <- (GeomPolygon)$draw(data=df.poly, scales=scales, coordinates=coordinates,...)
-        }
-        
-        #Paths, to immitate the border
-        pathgrob   <- (GeomPath)$draw(data=df, scales=scales, coordinates=coordinates,...)
-      }
-      
-      #Done
-      list(polygrob,pathgrob)
-    })
-    
-    # Ensure al least minimum grobs are available to return
-    if(length(contours) == 0){ contours = list(.zeroGrob) }
-    
-    #Put in gtree format and return
-    gTree(children = do.call("gList",unlist(contours,recursive=F)))
+    return(GeomPolygonTern$draw(.,data   = data,scales=scales,coordinates=coordinates,
+                                arrow    = arrow,
+                                lineend  = lineend,
+                                linejoin = linejoin,linemitre=linemitre,na.rm=na.rm,...))
   }
-  default_stat <- function(.) StatConfidence
-  required_aes <- c("x", "y")
-  default_aes <- function(.) aes(colour="black", size=0.5,linetype=2,alpha = NA,fill=NA)
-  guide_geom <- function(.) "path"
 })
 
 StatConfidence <- proto(ggint$Stat, {
--- R/geom-density-tern.R
+++ R/geom-density-tern.R
@@ -35,52 +35,24 @@ GeomDensityTern <- proto(ggint$GeomPath,{
   objname <- "density_tern"
   default_stat <- function(.) StatDensityTern
   default_aes  <- function(.) aes(colour="#3366FF",size = 0.5,linetype = 1, alpha = NA,weight=1,fill="transparent")
-  draw         <- function(., data,scales,coordinates,...){
+  draw         <- function(., data,
+                           scales,
+                           coordinates,
+                           arrow     = NULL,
+                           lineend   = "butt",
+                           linejoin  = "round",
+                           linemitre = 1,
+                           na.rm     = FALSE,
+                           ...){
     
     ##DO THE VARIABLE AESTHETIC CHECK x and y for cartesian, and x,y,z for ternary...
     req.aes <- unique(c(.$required_aes,coordinates$required_aes))
     check_required_aesthetics(req.aes, names(data),.$objname)
-    data <- remove_missing(data, na.rm = na.rm, required_aes,name = .$objname)
-      
-    #Check
-    if(!inherits(coordinates,"ternary")){
-      warning("Coordinates Must be Ternary, returning zerogrob")
-      return(.zeroGrob)
-    }
     
-    #Which to Ply On 
-    plyon = c("PANEL","group")
-    
-    ##Build the List of Grobs in Sequence
-    contours = dlply(data,plyon,function(df.orig){
-      
-      #Kill the Colours
-      df  <- suppressColours(df.orig,coordinates,remove=FALSE)
-      
-      #Default Grobs
-      polygrob = pathgrob = .zeroGrob
-      
-      if(nrow(df) > 0){
-        #Polygons, with no border (color)
-        df.poly <- subset(df,!(fill == 'transparent') | fill == NA)
-        if(nrow(df.poly) > 0){
-          df.poly$colour <- NA
-          polygrob <- (GeomPolygon)$draw(data=df.poly, scales=scales, coordinates=coordinates,...)
-        }
-        
-        #Paths, to immitate the border
-        pathgrob   <- (GeomPath)$draw(data=df, scales=scales, coordinates=coordinates,...)
-      }
-      
-      #Done
-      list(polygrob,pathgrob)
-    })
-    
-    # Ensure al least minimum grobs are available to return
-    if(length(contours) == 0){ contours = list(.zeroGrob) }
-    
-    #Put in gtree format and return
-    gTree(children = do.call("gList",unlist(contours,recursive=F)))
+    return(GeomPolygonTern$draw(.,data   = data,scales=scales,coordinates=coordinates,
+                                arrow    = arrow,
+                                lineend  = lineend,
+                                linejoin = linejoin,linemitre=linemitre,na.rm=na.rm,...))
   }
 })
 
